e8f6c8da2291902171915c118dc0a844facfa71a
CrisCW1990 <38779980+CrisCW1990@users.noreply.github.com>
2025-05-22 12:47:34 -0500

    Update AndroidProducts.mk
    Signed-off-by: CrisCW1990 <38779980+CrisCW1990@users.noreply.github.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,17 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 The TWRP Open Source Project
+# Copyright (C) 2024 OrangeFox Recovery Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_xun.mk
+    $(LOCAL_DIR)/twrp_xun.mk \
+    $(LOCAL_DIR)/orangefox_xun.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_xun-user \
     twrp_xun-userdebug \
-    twrp_xun-eng
+    twrp_xun-eng \
+    orangefox_xun-userdebug \
+    orangefox_xun-eng
